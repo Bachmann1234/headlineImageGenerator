0d31a20b58d0ff6e4d8aaa42a4ebb86593094fcb
Matt Bachmann <bachmann.matt@gmail.com>
2023-02-21 21:55:13 -0500

    Digital art seems better than realistic

--- main.go
+++ main.go
@@ -10,12 +10,12 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	headline := getRandomPopularNYTArticle()
 	log.Print(headline)
-	err := generateImage(headline)
+	err := generateImage(headline + ", digital art")
 	if err != nil {
 		log.Print("Headline denied by DALLE2")
 		censoredHeadline := censorHeadline(headline)
 		log.Print(censoredHeadline)
-		err := generateImage(censoredHeadline)
+		err := generateImage(censoredHeadline + ", digital art")
 		if err != nil {
 			panic(err)
 		}
